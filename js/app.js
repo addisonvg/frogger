f1b407076c863a195d5d837a9e93072b35a73348
addisonvg <addisonvangulden@gmail.com>
2015-04-06 11:50:40 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -22,16 +22,18 @@ Enemy.prototype.update = function(dt) {
 }
 
 // ititiate Enemy and assign coordinate + speed range
-// TODO: add multipliers for enemy speed per win
 Enemy.prototype.init = function(){
     this.x = getRandomVal(-350, -50);
     this.y = enemyY[Math.floor(Math.random() * enemyY.length)];
     this.speed = getRandomVal(100, 250)
+    if (this.wins == 5) {
+                this.speed = getRandomVal(250, 450);
+                alert ("Y ou are good at this, lets make things harder!");
+            };
 }
 
 // Assign y cordinanates to hold Enemies in Rows
 var enemyY = [55, 140, 225, 310];
-//var enemyX = [-350, -325, -300, -275, -250, -225, -200, -175, -150]
 
 // Randomizer function for Enemy
 function getRandomVal(min, max) {
@@ -41,18 +43,22 @@ function getRandomVal(min, max) {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    
 }
 
 // Calls on Enemy collison
 Enemy.prototype.onCollision = function(player){
-    player.loses += 1;
+    player.health -= 1;
     player.reset();
     this.reset();
+    alert ("Ouch that Hurt :( try again")
 }
 
 // Reset point for enemies on collison 
 Enemy.prototype.reset = function() {
-    this.init();
+    this.x = getRandomVal(-350, -50);
+    this.y = enemyY[Math.floor(Math.random() * enemyY.length)];
+    this.speed = getRandomVal(100, 250)  
 }
 
 
@@ -66,42 +72,41 @@ var Player = function(){
     //add and place player
     this.sprite = 'images/char-horn-girl.png';
     this.init();
-    //this.x = 200;
-    //this.y = 300;
     this.wins = 0;
-    this.loses = 0;
+    this.health = 3;
     
 }
 //
 Player.prototype.init = function(){
     this.x = 200;
     this.y = 400;
-//    this.wins = 1;
-//    this.loses = 1;
 }
 
+//create score board
 Player.prototype.renderScore = function(){
-   ctx.font = '200 18pt Arial' ;
+   ctx.font = '200 18pt Arial';
    ctx.clearRect(0,0, 500, 40);
-   ctx.fillText("Ladies Be Buggin'", 10, 40);
+   ctx.fillText("Enemies: " + E, 10, 40);
    ctx.fillText("Wins: " + this.wins, 300, 40 );
-   ctx.fillText("loses: " + this.loses, 400, 40);
+   ctx.fillText("health: " + this.health, 400, 40);
 }
 
+//player movement
 Player.prototype.handleInput = function(direction){
     
-    //if(direction == "space") {
-    //       this.reset();
-    //    }
+    //spacebar resets player
+    if (direction == "space" && this.x < 315) 
+           this.x = 200, this.y = 400;
     
+    //directional arrows for player        
     if (direction == 'left' && this.x > 0)
         this.x = this.x - 101;
     if (direction == 'right' && this.x < 404)
         this.x = this.x + 101;
     if (direction == 'up' && this.y >0 )
-        this.y = this.y - 83;
+        this.y = this.y - 82;
     if (direction == 'down' && this.y < 407)
-        this.y = this.y + 83;
+        this.y = this.y + 82;
 }
 
 // render Player
@@ -114,84 +119,77 @@ Player.prototype.update = function(dt){
     this.checkCollisions(dt);
 }
 
-// Create PLayer Reset point after collision
-Player.prototype.reset = function(){
+// Create Player Reset point after collision
+Player.prototype.reset = function(){ 
     this.x = 200;
-    this.y = 400;   
+    this.y = 400;
+    //alert notice on loses
+    if (this.health == 0 ) {
+        this.wins = 0;
+        this.health = 3;
+        alert ("Quit Buggin' you Lose!");
+    }
 }
 
-
+var E = 9;
 //Check Collisons, Enemy Collisions
 Player.prototype.checkCollisions = function(dt){
     for(var i in allEnemies) {
-        if( Math.abs(this.x - allEnemies[i].x) <= 40
-         && Math.abs(this.y - allEnemies[i].y) <= 40){
-            allEnemies[i].onCollision(this);
-            alert ("Quit Buggin' and be gone!");
-            //this.reset(alert);
-            
-            
+        if( Math.abs(this.x - allEnemies[i].x) <= 30
+         && Math.abs(this.y - allEnemies[i].y) <= 30){
+            allEnemies[i].onCollision(this);          
         }
+        
         // Use Collision with Water to asses "wins"
-        else if (this.y <= 60)
+        else if (this.y <= 20)
           {
-            /*var alert = window.alert ("now we are swimming, you win!")*/;
             this.reset();
             this.wins += 1;
-            alert ("now we are swimming, you win!")
-            //break;
+            
+            alert ("now we are swimming, you win!");
+            if (this.wins >= 3) {
+                E += 2;
+                enemyIncrease();
+                };
+            if (this.wins == 3) {
+                alert ("You are good at this, lets make things harder!");
+
+            };           
         }
     }
 }
-    //if( Math.abs(this.x - gem.x) <= 40 && Math.abs(this.y - gem.y) <= 40) {
-    //    this.score = this.score + 20;
-    //    //this.renderScoreLine();
-    //    gem.init();
-    //}
-    //
-    //if (this.y <= 10) {
-    //    this.next();
-    //}
-    //if( Math.abs(this.x - heart.x) <= 40 && Math.abs(this.y - heart.y) <= 40) {
-    //
-    //    heart.init();
-    //    heart.x = -100;
-    //    heart.y = -100;
-    //
-    //
-    //    if (this.life < 5){
-    //        this.life +=1;
-    //    }
-    //}
-    
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var Line = [65, 148, 231]; 
+ 
 var allEnemies = [];
-for(var i= 0; i < 9; i++){
-    allEnemies.push( new Enemy());  
-}
-var player = new Player();
 
-//var Water = function(){
-//    this.sprite = 'images/water-block.png';
-//    this.init();
-//    
-//}
-//
-//Water.prototype.init = function(){
-//    this.y = this.y;
-//    this.x = this.x;
-//}
+
+var enemyLoop = function (){
+    for(var i = 0; i < E; i++){
+    allEnemies.push( new Enemy());
+     if (this.y <= 20) break;
+    
+    }
+}
+enemyLoop();
+
+var enemyIncrease = function (){
+    for(var i= 0; i < 2; i++){
+        allEnemies.push(new Enemy());
+    }
+}
+
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
+        32: 'space',
         37: 'left',
         38: 'up',
         39: 'right',
